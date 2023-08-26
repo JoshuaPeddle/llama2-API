5abea3f2ea69d56cb53249c60068535895f02de5
Joshua Peddle <joshuapeddle128@gmail.com>
2023-08-26 20:47:07 -0230

    Initial commit of api

---
+++ models/README.md
@@ -0,0 +1,14 @@
+# Intro
+
+This models/ folder is empty other than this README.md file when you clone the repo.
+You must request access from facebookresearch to download the models.
+
+### Place any one of the following models in this folder:
+
+* Llama-2-7b-chat
+* Llama-2-13b-chat
+* Llama-2-70b-chat
+
+### Diffent models are used for diffrent functions
+
+You need one chat model to chat with the bot.
---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import os
+os.environ['RANK'] = '0'
+os.environ['WORLD_SIZE'] = '1'
+os.environ['MASTER_ADDR'] = '127.0.0.1'
+os.environ['MASTER_PORT'] = '5005'
+
+import torch.distributed as dist
+dist.init_process_group("nccl")
+from llama import Llama
+
+app = Flask(__name__)
+
+# Load the model
+generator = Llama.build(
+    ckpt_dir='models/llama-2-7b-chat/',
+    tokenizer_path='llama/tokenizer.model',
+    max_seq_len=128,
+    max_batch_size=4,
+)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    print(data)
+    dialog = data['dialog']
+    max_gen_len =int(data.get('max_gen_len'))
+    temperature = data.get('temperature', 0.6)
+    top_p = data.get('top_p', 0.9)
+    
+    results = generator.chat_completion(
+        [[dialog]],
+        max_gen_len=max_gen_len,
+        temperature=temperature,
+        top_p=top_p
+    )
+    
+    return jsonify(results[0])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, use_reloader=False)
